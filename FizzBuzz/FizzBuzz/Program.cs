b94bf51e3feb56d21a6b6be96047e073a0303caa
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-18 14:54:26 +0100

    command line parameter done commit

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -4,30 +4,62 @@ namespace FizzBuzz
 {
     class Program
     {
+        class Flags
+        {
+            public bool three = false;
+            public bool five = false;
+            public bool seven = false;
+            public bool eleven = false;
+            public bool thirteen = false;
+            public bool seventeen = false;
+        }
+        Flags setFlags(string[] args)
+        {
+            Flags flags = new Flags();
+            for(int i=0;i<args.Length;i++)
+            {
+                if (args[i] == "3")
+                    flags.three = true;
+                else if (args[i] == "7")
+                        flags.seven = true;
+                else if (args[i] == "5")
+                    flags.five = true;
+                else if (args[i] == "11")
+                    flags.eleven = true;
+                else if (args[i] == "13")
+                    flags.thirteen = true;
+                else if (args[i] == "17")
+                    flags.seventeen = true;
+            }
+            return flags;
+        }
+
         static void Main(string[] args)
         {
+            var program = new Program();
+            Flags flags = program.setFlags(args);
             Console.WriteLine("Write maximum number : ");
             string max;
             max = Console.ReadLine();
             for (int i = 1; i <= Int64.Parse(max); i++) 
             {
                 string message = "";
-                if (i % 3 == 0)
+                if (i % 3 == 0 && flags.three)
                 {
-                    if (i % 5 == 0)
+                    if (i % 5 == 0 && flags.five)
                         message = "FizzBuzz";
                     else
                         message = "Fizz";
                 }
-                else if (i % 5 == 0)
+                else if (i % 5 == 0 && flags.five)
                     message = "Buzz";
-                if (i % 7 == 0)
+                if (i % 7 == 0 && flags.seven)
                     if (String.IsNullOrEmpty(message))
                         message = "Bang";
                     else message += "Bang";
-                if (i % 11 == 0)
+                if (i % 11 == 0 && flags.eleven)
                     message = "Bong";
-                if(i % 13 ==0)
+                if(i % 13 ==0 && flags.thirteen)
                     if (String.IsNullOrEmpty(message))
                         message = "Fezz";
                     else if (!message.Contains('B'))
@@ -39,7 +71,7 @@ namespace FizzBuzz
                         string second_message = message.Substring(index, message.Length - first_message.Length);
                         message = first_message + "Fezz" + second_message;
                     }
-                if (i % 17 == 0 && !String.IsNullOrEmpty(message)) 
+                if (i % 17 == 0 && !String.IsNullOrEmpty(message) && flags.seventeen) 
                 {
                     string reversed_message = "";
                     int pos = message.Length;
