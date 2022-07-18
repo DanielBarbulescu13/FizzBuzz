c7d9c7953c5a5ce31b943613a62b9f29cd30a9ae
Daniel Barbulescu <Daniel.Barbulescu@softwire.com>
2022-07-18 12:59:39 +0100

    3rd commit

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -6,8 +6,19 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            for(int i=1;i<=100;i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                        Console.WriteLine("FizzBuzz");
+                    else Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
         }
     }
 }
